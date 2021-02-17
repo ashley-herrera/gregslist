0c3b4a5bc16c7fb3ab93b3c53e9cbfc0e893dd25
Ashley Herrera <ashleyherrera131@gmail.com>
2021-02-17 16:18:23 -0700

    replaced image

--- app/AppState.js
+++ app/AppState.js
@@ -9,7 +9,7 @@ class AppState extends EventEmitter {
   values = []
   //NOTE adding a type to your collections with jsdocs gives additional intellisense when referencing that collection.
   /**@type {Car[]} */
-  cars = [new Car({make: "Jeep", model: "Wrangler", price: 20, imgUrl: 'http://images.thetruthaboutcars.com/2011/11/Wrangler-front-quarter.jpg', year: 2012, description: "Its nice", miles: 75000}), new Car({make: "Jeep", model: "Rango", price: 1500, imgUrl: 'http://images.thetruthaboutcars.com/2011/11/Wrangler-front-quarter.jpg', year: 2012, description: "Its very nice", miles: 5000})]
+  cars = [new Car({make: "Jeep", model: "Wrangler", price: 20, imgUrl: 'http://images.thetruthaboutcars.com/2011/11/Wrangler-front-quarter.jpg', year: 2012, description: "Its nice", miles: 75000}), new Car({make: "Jeep", model: "Rango", price: 1500, imgUrl: 'https://i.pinimg.com/originals/9a/99/57/9a9957249a8f1f12d8711e9403d87278.jpg', year: 2012, description: "Its very nice", miles: 5000})]
 
   /**@type {House[]} */
   houses = [new House({homeType:"Apartment", beds:"Beds " + 1, bathrooms:"Bathroom " + 1, sqft: 900, price: 1500, imgUrl:'http://cdn.home-designing.com/wp-content/uploads/2015/10/wood-paneling-apartment.jpg'}), new House({homeType:"Condo", beds:"Beds " + 3, bathrooms:"Bathroom " + 2, sqft: 1378, price: 250000, imgUrl:'https://photos.zillowstatic.com/fp/4410c13274f1ad6ebe51ee7950cbc3de-cc_ft_768.jpg'})]
