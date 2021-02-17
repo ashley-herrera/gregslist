fe5686cf11daeb002076cbd3310b9f3a87a1d1cc
Ashley Herrera <ashleyherrera131@gmail.com>
2021-02-17 14:43:21 -0700

    draw function

--- app/AppState.js
+++ app/AppState.js
@@ -10,8 +10,8 @@ class AppState extends EventEmitter {
   /**@type {Car[]} */
   cars = [new Car({make: "Jeep", model: "Wrangler", price: 20, imgUrl: 'http://images.thetruthaboutcars.com/2011/11/Wrangler-front-quarter.jpg', year: 2012, description: "Its nice", miles: 75000}), new Car({make: "Jeep", model: "Rango", price: 1500, imgUrl: 'http://images.thetruthaboutcars.com/2011/11/Wrangler-front-quarter.jpg', year: 2012, description: "Its very nice", miles: 5000})]
 
-  // /**@type {House[]} */
-  // houses = [new House({})]
+  /**@type {House[]} */
+  houses = [new House({})]
 }
 
 export const ProxyState = new Proxy(new AppState(), {
--- app/Controllers/HouseController.js
+++ app/Controllers/HouseController.js
@@ -1,4 +1,12 @@
+import { ProxyState } from "../AppState.js"
 
+
+
+function _draw(){
+    let house = ProxyState.houses
+    let template = ""
+    house.forEach(house=> template += house.Template)
+}
 
 export default class HouseController{
     constructor(){
