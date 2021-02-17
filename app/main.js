e7ce68408369c7ef1033c2325229ccb69d562c8b
Ashley Herrera <ashleyherrera131@gmail.com>
2021-02-17 14:18:17 -0700

    minor edits

--- app/main.js
+++ app/main.js
@@ -5,6 +5,7 @@ import ValuesController from "./Controllers/ValuesController.js";
 class App {
   // valuesController = new ValuesController();
   carsController = new CarsController();
+  houseController = new HouseController();
 }
 
 window["app"] = new App();
