e1ca760f3e4f2d9e65a4a4b61f414f5a2ccfecfc
Ashley Herrera <ashleyherrera131@gmail.com>
2021-02-23 14:29:33 -0700

    removed miles

--- app/Models/Car.js
+++ app/Models/Car.js
@@ -1,13 +1,12 @@
 
 export default class Car{
-constructor({make, model, year, price, description, imgUrl, miles, _id, id}){
+constructor({make, model, year, price, description, imgUrl, _id, id}){
   this.make = make
   this.model = model
   this.year = year
   this.price = price
   this.description = description
   this.imgUrl = imgUrl
-  this.miles = miles
   this.id = _id || id
 }
 
@@ -18,7 +17,6 @@ get Template(){
   <div class="card-body">
       <h4 class="card-title">${this.year} ${this.make} - ${this.model}</h4>
       <p class="card-text">${this.description}</p>
-      <p>Miles : ${this.miles}</p>
       <p>Price: ${this.price}</p>
       <button class="btn btn-success" onclick="app.carsController.bid('${this.id}')">Bid</button>
   </div>
